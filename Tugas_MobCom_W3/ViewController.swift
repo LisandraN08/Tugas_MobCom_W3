65349deae90228e4fffe87dfebbe2a2f2636513e
LisandraN08 <lnicoline@student.ciputra.ac.id>
2023-10-06 12:32:15 +0700

    Initial Commit

---
+++ Tugas_MobCom_W3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tugas_MobCom_W3
+//
+//  Created by MacBook Pro on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
